dd5fede0da01ac05b0f64110fd952bc489ddc273
Anthony <antonio.s.p@yandex.ru>
2024-10-29 16:24:57 +0300

    Create module_5_3.py

---
+++ module_5_3.py
@@ -0,0 +1,69 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        '''new_floor - номер этажа(int), на который нужно приехать'''
+        for i in range(1, new_floor):
+            print(i)
+        if self.number_of_floors < new_floor or self.number_of_floors < 1:
+            print('Такого этажа не существует')
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        '''__eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.'''
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+
+    def __radd__(self, other):
+        self.number_of_floors += value
+
+    def __iadd__(self, other):
+        self.number_of_floors += value
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
